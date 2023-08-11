f04feb0012343e5e5658de9e3a77acdb9bff9951
Halohunterx234 <ultrahalogt@gmail.com>
2023-08-11 17:38:33 +0800

    working on pause screen rn pt1

--- Assets/Scenes/Scene scripts/MenuController.cs
+++ Assets/Scenes/Scene scripts/MenuController.cs
@@ -7,11 +7,13 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public BookFlip bk;
+    public GameObject MainScreen, WeaponsScreen, ItemsScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-       // bk.SetActive(false);
+        // bk.SetActive(false);
+        GoToMain();
     }
 
     // Update is called once per frame
@@ -23,15 +25,42 @@ public class MenuController : MonoBehaviour
     //pause game and open up the book
     public void BackToTitle()
     {
+        UnActivePages();
         Time.timeScale = 1;
         bk.Exit();
         SceneManager.LoadScene("New title scene");
     }
 
+    //unpause game and go back to the level
     public void BackToGame()
     {
+        UnActivePages();
         Time.timeScale = 1;
         bk.Exit();
     }
 
+    //unactive pages
+    public void UnActivePages()
+    {
+        MainScreen.SetActive(false);
+        WeaponsScreen.SetActive(false);
+        ItemsScreen.SetActive(false);
+    }
+    //swap pages
+    public void GoToMain()
+    {
+        UnActivePages();
+        MainScreen.SetActive(true);
+    }
+    public void GoToWeapon()
+    {
+        UnActivePages();
+        WeaponsScreen.SetActive(true);
+    }
+    public void GoToItems()
+    {
+        UnActivePages();
+        ItemsScreen.SetActive(true);
+    }
+    
 }
